b186d3b0e944c5bbf9e1cf6ebf68225e96de2ab9
mukeshsham <113046564+mukeshsham@users.noreply.github.com>
2022-09-29 21:59:18 +0530

    Add files via upload

---
+++ lab/multipleinheritance.java
@@ -0,0 +1,20 @@
+//NAME :MUKESH S
+//BATCH: 2576
+//write a program to multiple inheritance
+class Red{
+void msg(){
+system.out.println("hello");
+}}
+class Green{
+void msg(){
+system.out.println("hi");
+}}
+public class main{
+class Yellow extends Red,Green{
+void msg(){
+system.out.println("how are you");
+}}
+public static void main(string[]args){
+Yellow obj=new (Yellow);
+obj.msg();
+}}
