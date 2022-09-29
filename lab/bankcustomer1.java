b03bc9d1b1c469c6d02843956b736a58dc78b926
mukeshsham <113046564+mukeshsham@users.noreply.github.com>
2022-09-29 22:02:08 +0530

    Add files via upload

---
+++ lab/bankcustomer1.java
@@ -0,0 +1,16 @@
+//NAME :MUKESH S
+//BATCH: 2576
+//package name "anudip1"
+//print the details from package "anudip"
+package anudip1;//package creation
+import anudip.Bankcustomer;//import the package from anudip &class name Bankcustomer1
+public class Bankcustomer1
+{
+	public static void main(String[] args)//main method
+	{
+		Bankcustomer b=new Bankcustomer();//object creation
+		b.get(765,"mukesh",55000);//give the input
+		b.show();//call the method
+	}
+}
+	
