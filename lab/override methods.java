93bc74155c3d6f08aa8408bb75cefc193a11fbac
mukeshsham <113046564+mukeshsham@users.noreply.github.com>
2022-10-07 21:56:01 +0530

    Add files via upload

---
+++ lab/override methods.java
@@ -0,0 +1,45 @@
+//NAME: MUKESH S
+//BATCH: 2576
+//method overriding
+class Bank//parent class
+{
+	int intr()//method overriding
+	{
+		return 0;
+	}
+}
+class Sbi extends Bank//child class 1
+{
+	int intr()//method overriding
+	{
+		return 7;
+	}
+}
+class Axis extends Bank//child class 2
+{
+	int intr()//method overriding
+	{
+		return 8;
+	}
+}
+class Icici extends Bank//child class 3
+{
+	int intr()//method overriding
+	{
+		return 9;
+	}
+}
+public class Bank
+{	
+    public static void main(String[] args)
+    {
+		Bank b=new Bank();//object creation
+		Sbi s=new Sbi();
+		Axis a=new Axis();
+		Icici i=new Icici();
+		System.out.println("The bank returns the interst is:" +b.intr());//method callig
+		System.out.println("The SBI bank returns the interst is:" +s.intr());
+		System.out.println("The Axis bank returns the interst is:" +a.intr());
+		System.out.println("The Icici bank returns the interst is:" +i.intr());
+	}
+}
